04435f14cf6fee43db2ef15d6ea980e42d541d25
Maxianrodrigues <maxianmuller2008@gmail.com>
2025-08-02 11:35:12 -0300

    FEAT CHECKOUT

--- cypress/support/pages/checkout_page.js
+++ cypress/support/pages/checkout_page.js
@@ -1,1 +1,28 @@
 ///<reference types="cypress" />
+const elementos = {
+
+buttons:{
+
+}
+
+}
+
+export default{
+
+    validarUrlStripe(urlstripe){
+        cy.window().then((win) => {
+            cy.stub(win, 'open').as('windowOpen');
+        });
+        cy.contains('button', 'Fazer Upgrade - R$ 29,90/mês').should('be.visible')
+            .should('be.enabled').click()
+
+        cy.get('@windowOpen').should('be.called');
+
+        cy.get('@windowOpen').then((stub) => {
+            const url = stub.getCall(0).args[0];
+
+            expect(url).to.include(urlstripe);
+            cy.log('URL GERADA CORRETAMENTE', url)
+        })
+    }
+}
